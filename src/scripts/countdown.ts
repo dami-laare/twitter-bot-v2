b366b846e80cf9bea9ec5f7dba8853f3e9e7cdf5
Temitayo <osunkiyesitayo@gmail.com>
2022-12-06 04:01:07 +0100

    udpate-again

--- src/scripts/countdown.ts
+++ src/scripts/countdown.ts
@@ -4,7 +4,7 @@ const jesusTwitterService = new JesusTwitterService();
 
 const rebootCampStartDate = new Date("2022-12-09");
 const todayDate = new Date();
-if (rebootCampStartDate.getTime() - todayDate.getTime()) {
+if (rebootCampStartDate.getTime() - todayDate.getTime() > 0) {
   jesusTwitterService.tweetCountDown().then((content) => {
     process.exit();
   });
